f91123c4bbef04f737caa18c9698ab9bbd8664b7
Enzo <158579506+bileromerson@users.noreply.github.com>
2024-02-29 16:59:00 -0300

    Add files via upload

---
+++ class-3/class 3-1.py
@@ -0,0 +1,8 @@
+for num in range(1,21):
+    print(num)
+
+for num in range(1,101):
+    if (num %2 == 0):
+        print(f'par{num}')
+    else:
+        print(f'inpar{num}')
---
+++ class-3/class 3-5.py
@@ -0,0 +1,10 @@
+
+num = int(input("digite um numero de 1 a 10: \n"))
+vezes = 0
+while num > 10 or num < 0:
+    print('o numero deve ser entr 1 e 10')
+    num = int(input("digite um numero de 1 a 10: \n"))
+else:
+    for n in range(1, 11):
+        vezes +=1
+        print(f'{num} x {vezes} = {num * n}')
